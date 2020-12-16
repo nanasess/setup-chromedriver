06a97fc8dcf7bdaae35a5d401bec3b18bd350c35
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2020-12-16 10:07:54 +0900

    Fix GitHub Actions

--- lib/setup-chromedriver.sh
+++ lib/setup-chromedriver.sh
@@ -5,6 +5,11 @@ set -eo pipefail
 VERSION=$1
 ARCH=$2
 
+if ! type -a google-chrome > /dev/null 2>&1; then
+    sudo apt-get update
+    sudo apt-get install -y google-chrome
+fi
+
 if [ "$VERSION" == "" ]; then
   CHROME_VERSION=$(google-chrome --version | cut -f 3 -d ' ' | cut -d '.' -f 1)
   VERSION=$(curl --location --fail --retry 10 http://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION})
