ff9f870ccc39aacd6aa8d847302a9afea0c36b23
Tom Ross <69170368+tomwhross@users.noreply.github.com>
2021-01-23 12:48:55 -0500

    Add support for macos-lastest build

--- lib/setup-chromedriver.sh
+++ lib/setup-chromedriver.sh
@@ -5,13 +5,19 @@ set -eo pipefail
 VERSION=$1
 ARCH=$2
 
-if ! type -a google-chrome > /dev/null 2>&1; then
-    sudo apt-get update
-    sudo apt-get install -y google-chrome
+if [ "$ARCH" == "linux64" ]; then
+    if ! type -a google-chrome > /dev/null 2>&1; then
+        sudo apt-get update
+        sudo apt-get install -y google-chrome
+    fi
 fi
 
 if [ "$VERSION" == "" ]; then
-  CHROME_VERSION=$(google-chrome --version | cut -f 3 -d ' ' | cut -d '.' -f 1)
+    if [ "$ARCH" == "mac64" ]; then
+        CHROME_VERSION=$(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version | cut -f 3 -d ' ' | cut -d '.' -f 1)
+    elif [ "$ARCH" == "linux64" ]; then
+        CHROME_VERSION=$(google-chrome --version | cut -f 3 -d ' ' | cut -d '.' -f 1)
+    fi
   VERSION=$(curl --location --fail --retry 10 http://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION})
 fi
 
