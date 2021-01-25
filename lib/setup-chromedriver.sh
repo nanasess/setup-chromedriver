5e90ca71b4381b7b26daa8c9b1b950abeee0441f
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2021-01-25 18:42:17 +0900

    Fix macos-latest

--- lib/setup-chromedriver.sh
+++ lib/setup-chromedriver.sh
@@ -6,19 +6,18 @@ VERSION=$1
 ARCH=$2
 
 if [ "$ARCH" == "linux64" ]; then
+    CHROMEAPP=google-chrome
     if ! type -a google-chrome > /dev/null 2>&1; then
         sudo apt-get update
         sudo apt-get install -y google-chrome
     fi
+elif [ "$ARCH" == "mac64" ]; then
+    CHROMEAPP="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 fi
 
 if [ "$VERSION" == "" ]; then
-    if [ "$ARCH" == "mac64" ]; then
-        CHROME_VERSION=$(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version | cut -f 3 -d ' ' | cut -d '.' -f 1)
-    elif [ "$ARCH" == "linux64" ]; then
-        CHROME_VERSION=$(google-chrome --version | cut -f 3 -d ' ' | cut -d '.' -f 1)
-    fi
-  VERSION=$(curl --location --fail --retry 10 http://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION})
+    CHROME_VERSION=$("$CHROMEAPP" --version | cut -f 3 -d ' ' | cut -d '.' -f 1)
+    VERSION=$(curl --location --fail --retry 10 http://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION})
 fi
 
 wget -c -nc --retry-connrefused --tries=0 https://chromedriver.storage.googleapis.com/${VERSION}/chromedriver_${ARCH}.zip
