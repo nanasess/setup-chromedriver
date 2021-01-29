2bcf6ec4f035e9560250e66cfbd042f7fdca3f39
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2021-01-29 18:03:38 +0900

    Improve support for Windows

---
+++ lib/setup-chromedriver.ps1
@@ -0,0 +1,16 @@
+Param(
+    [string]$version
+)
+
+if([string]::IsNullOrEmpty($version))
+{
+    $chrome_fullversion = (Get-Item "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe").VersionInfo.FileVersion
+    $chrome_majorversion = $chrome_fullversion.Split[0]
+    $response = Invoke-WebRequest "http://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chrome_majorversion"
+    $version = $response.Content
+}
+
+Invoke-WebRequest "https://chromedriver.storage.googleapis.com/$version/chromedriver_win32.zip" -OutFile chromedriver_win32.zip
+
+Expand-Archive -Path chromedriver_win32.zip -DestinationPath C:\SeleniumWebDrivers\ChromeDriver -Force
+Remove-Item chromedriver_win32.zip
