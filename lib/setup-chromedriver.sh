9dafc37b324e9954788e3cb502dee5e2f517ebd4
mvdbeek <m.vandenbeek@gmail.com>
2023-07-26 11:43:44 +0200

    Use @puppeteer/browsers to install chromedriver

--- lib/setup-chromedriver.sh
+++ lib/setup-chromedriver.sh
@@ -16,11 +16,9 @@ elif [ "$ARCH" == "mac64" ]; then
 fi
 
 if [ "$VERSION" == "" ]; then
-    CHROME_VERSION=$("$CHROMEAPP" --version | cut -f 3 -d ' ' | cut -d '.' -f 1)
-    VERSION=$(curl --location --fail --retry 10 http://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION})
+    VERSION=$("$CHROMEAPP" --version | cut -f 3 -d ' ')
 fi
 
-wget -c -nc --retry-connrefused --tries=0 https://chromedriver.storage.googleapis.com/${VERSION}/chromedriver_${ARCH}.zip
-unzip -o -q chromedriver_${ARCH}.zip
-sudo mv chromedriver /usr/local/bin/chromedriver
-rm chromedriver_${ARCH}.zip
+npx @puppeteer/browsers install "chromedriver@$VERSION"
+sudo mv chromedriver/*/*/chromedriver /usr/local/bin/chromedriver
+rm -rf chromedriver/
