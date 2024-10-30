cd1684c949e76bc91b99280100981fcac6ed5810
MickaelFontes <81414455+MickaelFontes@users.noreply.github.com>
2024-10-30 19:56:05 +0100

    feat: :sparkles: Add `CHROMEAPP` input variable
    - add CHROMEAPP variable for Linux and MacOS Google Chrome binary name

--- src/setup-chromedriver.ts
+++ src/setup-chromedriver.ts
@@ -6,6 +6,7 @@ async function run() {
   try {
     console.log(`##setup chromedriver`);
     const version = core.getInput("chromedriver-version", { required: false });
+    const chromeapp = core.getInput("chromeapp", { required: false });
     const plat = process.platform;
     let arch = "linux";
     switch (plat) {
@@ -22,12 +23,13 @@ async function run() {
     if (arch == "win32") {
       await exec.exec(
         "powershell -File " +
-          path.join(__dirname, "../lib", "setup-chromedriver.ps1 " + version)
+        path.join(__dirname, "../lib", "setup-chromedriver.ps1 " + version)
       );
     } else {
       await exec.exec(path.join(__dirname, "../lib", "setup-chromedriver.sh"), [
         version,
         arch,
+        chromeapp
       ]);
     }
   } catch (error: unknown) {
